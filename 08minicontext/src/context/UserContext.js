5859f781583aee6c6f18f7cab3f9e96976d770fd
Shivam5698 <shivammauryashivammaurya615@gmail.com>
2026-09-03 21:09:45 +0530

    minicontext

---
+++ 08minicontext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const UserContext=React.createContext()
+
+export default UserContext;
+//context apne sath provider lata hai matlab hmm sabhi component jo iss variable ko wrap kar denge taabhi vo uss variable ka use kar payenge//
