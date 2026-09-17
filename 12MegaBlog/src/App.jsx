cb166df68ec2bfa7f81977c91eb2a1a11f805b28
Shivam5698 <shivammauryashivammaurya615@gmail.com>
2026-09-17 12:32:11 +0530

    logout handler bnaya

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,39 @@
-// import { useState } from 'react'
-import './App.css'
+ import { useState,useEffect} from 'react'
+ import { useDispatch } from 'react-redux'
+ import './App.css'
+ import authService from './appwrite/auth'
+ import {login,logout} from "./store/authSlice"
+ import Header from "./components/Header/Header.jsx"
+ import Footer from "./components/Footer/Footer.jsx"
+ 
 
 function App() {
-  // const [count, setCount] = useState(0)
- console.log( import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <h1>Mega Blog</h1>
-    </>
-  )
-}
+   const [loading,setLoading] = useState(true)
+   const dispatch=useDispatch()
 
+   useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+      dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+   })
+ return !loading ? (
+     /* Yahan text-center add kiya hai aur sir ka original layout rakha hai */
+     <div className="min-h-screen flex flex-wrap content-between bg-gray-400 text-center mt-10">
+       <div className='w-full block'>
+         <Header/>
+         <main>
+          TODO: {/* outlet */}
+         </main>
+         <Footer/>
+       </div>
+     </div>
+  ) : null
+}
 export default App
